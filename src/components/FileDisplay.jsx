1b678acbf82f6190be9b2df81007e37b8b2eb22d
Zura Shukakidze <zurashukakidze85@gmail.com>
2025-01-13 18:09:05 +0400

    first commit

---
+++ src/components/FileDisplay.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function FileDisplay(props) {
+  const { file, audioStream, handleAudioReset } = props
+
+  return (
+    <main className='flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4
+    md:gap-5 justify-center pb-20'>
+    <h1 className='font-semibold text-4xl sm:text-5xl
+      md:text-6xl'>Your <span className='text-blue-400 bold'>File</span></h1>
+    </main>
+    
+  )
+}
